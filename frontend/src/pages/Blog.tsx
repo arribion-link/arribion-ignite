2a4b1307ad7366e56cc33feea2e99fc87b6ead26
arribion <arribion@gmail.com>
2025-09-07 12:54:15 +0300

    structure courses and blog page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,14 +1,17 @@
 const Blog = () => {
   return (
     <section>
-      <div className="blog-container">
+      <div className="blog-main-container">
         <div className="blog-page-header">
-          <h1>- Tech Talk Today</h1>
+          <h1>Talk Tech Today, With Insigts form Experts.</h1>
           <p>
             The intersection of ideas, intelligence, and innovation.
             <br />
             Navigating the fast lane of digital evolution
           </p>
+        </div>
+        <div className="blogs-container">
+
         </div>
       </div>
     </section>
--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -1,12 +1,51 @@
 
 const Courses = () => {
   return (
-    <section>
-      <>
-        <h1>Hello world</h1>
-      </>
+    <section className="courses-main-page">
+      <div className="courses-header"></div>
+      <div className="courses-main-container">
+        <aside>
+          <h1>
+            Get What your looking for first <br />
+            Filter Courses with ease!
+          </h1>
+
+          <h2>Programming Language</h2>
+          <select name="" id="">
+            <option value="javascript">Javascript</option>
+            <option value="typescript">Typescript</option>
+            <option value="java">Java</option>
+            <option value="dart">Dart</option>
+            <option value="python">Python</option>
+          </select>
+          <h2>FrameWorks & Libraries</h2>
+          <select name="" id="">
+            <option value="reactjs">React.js</option>
+            <option value="nextjs">Next.js</option>
+            <option value="astro">Astro</option>
+            <option value="react-native">React Native(EXPO)</option>
+            <option value="flutter">Flutter</option>
+            <option value="springBoot">Spring Boot</option>
+          </select>
+          <h2>Developer role</h2>
+          <select name="" id="">
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+            <option value="fullstack">FullStack</option>
+          </select>
+          <h2>Developer</h2>
+          <select name="" id="">
+            <option value="webdevelopment">Web development</option>
+            <option value="mobiledevelopment">Mobile development</option>
+            <option value=""></option>
+          </select>
+        </aside>
+        <div className="courses-container">
+          <input type="search" placeholder="Search for Courses..." />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Courses
